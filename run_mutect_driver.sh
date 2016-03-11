38715016752fd24a2b1aaf0ad0436460599838ec
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-03-11 15:25:53 -0600

    update

--- run_mutect_driver.sh
+++ run_mutect_driver.sh
@@ -17,11 +17,11 @@ PARCELIP='172.16.128.7';        # -rp
 INDEXYAMLPATH="~/mutect2-pon-cwl/tools/cramtools.cwl.yaml"; # -y
 
 # vars for run_mutect_docker_calling.sh
-CALLINGYAML="~/mutect2-pon-cwl/workflows/mutect2-pon-workflow.cwl.yaml" # -y
-SNPPATH="~/mutect_ref_files/dbsnp_144.grch38.vcf"                       # -s
-FAIPATH="~/mutect_ref_files/Homo_sapiens_assembly38.fa.fai"             # -i
-DICTPATH="~/mutect_ref_files/Homo_sapiens_assembly38.dict"              # -k
-COSMICPATH="~/mutect_ref_files/CosmicCombined.srt.vcf";                 # -x
+CALLINGYAML="/mnt/mutect2-pon-cwl/workflows/mutect2-pon-workflow.cwl.yaml" # -y
+SNPPATH="/mnt/mutect_ref_files/dbsnp_144.grch38.vcf"                       # -s
+FAIPATH="/mnt/mutect_ref_files/Homo_sapiens_assembly38.fa.fai"             # -i
+DICTPATH="/mnt/mutect_ref_files/Homo_sapiens_assembly38.dict"              # -k
+COSMICPATH="/mnt/mutect_ref_files/CosmicCombined.srt.vcf";                 # -x
 BLOCKSIZE="50000000";                                                   # -b
 THREADCOUNT==`grep -c "processor" /proc/cpuinfo`;                       # -t
 
@@ -103,34 +103,34 @@ for i in `cat list`; do
     #############################################################################################################
     
     # From Amazon with parcel
-    S3_W_PARCELDLSTARTTIME=`date +%s.%N`;
-    CMD="ARK_download.gamma.py -a $i -p $URLPATTERN2 -up -rp $PARCELIP -d";
-    echo $CMD >> $LOG;
-    eval $CMD &>>$LOG;
-    CMD_STATUS=$?;
-    echo -e "Command status: \t"$CMD_STATUS >> $LOG;
-    S3_W_PARCELDLENDTIME=`date +%s.%N`;
-    S3_W_ELAPSEDTIME=`echo "$S3_W_PARCEL_DL_ENDTIME - $S3_W_PARCEL_DL_STARTTIME" | bc -l`;
-    echo -e "Command runtime: \t"$S3_WO_ELAPSEDTIME >> $LOG;
-    FILE=`basename $i`;
-    S3_W_MD5=`md5sum $FILE`;
-    echo -e "md5: \t"$S3_W_MD5 >> $LOG;
-    rm $FILE;
+    # S3_W_PARCELDLSTARTTIME=`date +%s.%N`;
+    # CMD="ARK_download.gamma.py -a $i -p $URLPATTERN2 -up -rp $PARCELIP -d";
+    # echo $CMD >> $LOG;
+    # eval $CMD &>>$LOG;
+    # CMD_STATUS=$?;
+    # echo -e "Command status: \t"$CMD_STATUS >> $LOG;
+    # S3_W_PARCELDLENDTIME=`date +%s.%N`;
+    # S3_W_ELAPSEDTIME=`echo "$S3_W_PARCEL_DL_ENDTIME - $S3_W_PARCEL_DL_STARTTIME" | bc -l`;
+    # echo -e "Command runtime: \t"$S3_WO_ELAPSEDTIME >> $LOG;
+    # FILE=`basename $i`;
+    # S3_W_MD5=`md5sum $FILE`;
+    # echo -e "md5: \t"$S3_W_MD5 >> $LOG;
+    # rm $FILE;
 
-    # From Amazon without parcel
-    S3_WO_PARCEL_DL_STARTTIME=`date +%s.%N`;
-    CMD="ARK_download.gamma.py -a $i -p $URLPATTERN2 -d";
-    echo $CMD >> $LOG;
-    eval $CMD &>>$LOG;
-    CMD_STATUS=$?;
-    echo -e "Command status: \t"$CMD_STATUS >> $LOG;
-    S3_WO_PARCEL_DL_ENDTIME=`date +%s.%N`;
-    S3_WO_ELAPSEDTIME=`echo "$S3_WO_PARCEL_DL_ENDTIME - $S3_WO_PARCEL_DL_STARTTIME" | bc -l`;
-    echo -e "Command runtime: \t"$S3_WO_ELAPSEDTIME >> $LOG;
-    FILE=`basename $i`;
-    S3_WO_MD5=`md5sum $FILE`;
-    echo -e "md5: \t"$S3_WO_MD5 >> $LOG;
-    rm $FILE;
+    # # From Amazon without parcel
+    # S3_WO_PARCEL_DL_STARTTIME=`date +%s.%N`;
+    # CMD="ARK_download.gamma.py -a $i -p $URLPATTERN2 -d";
+    # echo $CMD >> $LOG;
+    # eval $CMD &>>$LOG;
+    # CMD_STATUS=$?;
+    # echo -e "Command status: \t"$CMD_STATUS >> $LOG;
+    # S3_WO_PARCEL_DL_ENDTIME=`date +%s.%N`;
+    # S3_WO_ELAPSEDTIME=`echo "$S3_WO_PARCEL_DL_ENDTIME - $S3_WO_PARCEL_DL_STARTTIME" | bc -l`;
+    # echo -e "Command runtime: \t"$S3_WO_ELAPSEDTIME >> $LOG;
+    # FILE=`basename $i`;
+    # S3_WO_MD5=`md5sum $FILE`;
+    # echo -e "md5: \t"$S3_WO_MD5 >> $LOG;
+    # rm $FILE;
     
     # From Grif with parcel
     GRIF_W_PARCELDLSTARTTIME=`date +%s.%N`;
@@ -210,7 +210,7 @@ for i in `cat list`; do
     ### Write nice formatted results to the stats file
     #############################################################################################################
     # echo -e "ark\tsample\ts3_w_parcel.dl_time\ts3_w_parcel.md5\ts3_wo_parcel.dl_time\ts3_wo_parcel.md5\tgrif_w_parcel.dl_time\tgrif_w_parcel.md5\tgrif_wo_parcel.dl_time\tgrif_wo_parcel.md5\tindexing.run_time\tcalling.run_time\n" >> $STATS
-    echo -e "$i\t$FILE\t$S3_W_ELAPSEDTIME\t$S3_W_MD5\t$S3_WO_ELAPSEDTIME\t$S3_WO_MD5\t$GRIF_W_PARCELDLENDTIME\t$GRIF_W_MD5\t$GRIF_WO_PARCEL_DL_ENDTIME\t$GRIF_WO_MD5\t$MUTECT1_ELAPSEDTIME\t$MUTECT2_ELAPSEDTIME" >> $STATS
+    #echo -e "$i\t$FILE\t$S3_W_ELAPSEDTIME\t$S3_W_MD5\t$S3_WO_ELAPSEDTIME\t$S3_WO_MD5\t$GRIF_W_PARCELDLENDTIME\t$GRIF_W_MD5\t$GRIF_WO_PARCEL_DL_ENDTIME\t$GRIF_WO_MD5\t$MUTECT1_ELAPSEDTIME\t$MUTECT2_ELAPSEDTIME" >> $STATS
     
 done    
 
