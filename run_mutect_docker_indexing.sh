3cca37cd291900668aa3697187db8aeef7d22ee5
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-03-14 12:45:29 -0500

    debug

--- run_mutect_docker_indexing.sh
+++ run_mutect_docker_indexing.sh
@@ -5,6 +5,7 @@ YAMLPATH="~/mutect2-pon-cwl/tools/cramtools.cwl.yaml";
 CRAMPATH="~/NA19771.alt_bwamem_GRCh38DH.20150826.MXL.exome.cram";
 FASTAPATH="~/mutect_ref_files/Homo_sapiens_assembly38.fa";
 UUID="uuid";
+DEBUG=0;
 
 # Parse input options
 while getopts ":y:c:f:u:dh" opt; do
