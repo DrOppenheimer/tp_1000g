b99279ba2a8fc2290c0e11159645f2ad672ee1f1
Kevin P Keegan <kevin@Kevins-Macintosh.local>
2016-04-28 13:30:24 -0500

    debug dl_ref_data.sh

--- dl_ref_data.sh
+++ dl_ref_data.sh
@@ -9,7 +9,9 @@ export http_proxy=http://cloud-proxy:3128
 export https_proxy=http://cloud-proxy:3128
 
 # check for ref data - download if it isn't there
-if [ ! -e $REF_FILE ]; then
+if [ -e $REF_FILE ]; then
+    echo "Ref data is already installed"
+else
     #sudo chown ubuntu:ubuntu /mnt                           # this way for VMs
     #mkdir -p /mnt/mutect_ref_files                          # this way for VMs
     #cd /mnt/mutect_ref_files                                # this way for VMs
@@ -18,9 +20,7 @@ if [ ! -e $REF_FILE ]; then
     cd ~/mutect_ref_files
     ARK_download.gamma.py -a 'https://signpost.opensciencedatacloud.org/alias/ark:/31807/DC2-39d246e3-c991-4368-a707-b74c19f16ce0' -d -p 'https://griffin-objstore' -b
     tar -xzvf 1000_genome_ref_files.3-7-16.tar.gz
-    echo "Done installing reference data"
-else
-    echo "Ref data is already installed"
+    echo "Done installing reference data"   
 fi
 
 unset http_proxy
